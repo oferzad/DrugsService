b2f09798471306550a20114f613c279f62494d14
Ofer Zadikario <ofer.zadikario@gmail.com>
2025-02-25 20:15:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using DrugsService.Serviices;
+using System.Dynamic;
+namespace DrugsService
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrugWebAPI api = new DrugWebAPI();
+            ExpandoObject input = new ExpandoObject();
+            input.TryAdd("val", "אקמול");
+            input.TryAdd("prescription", false);
+            input.TryAdd("healthServices", false);
+            input.TryAdd("pageIndex", 1);
+            input.TryAdd("orderBy", 0);
+
+            ExpandoObject result = api.SearchByName(input).Result;
+            if (result != null)
+            {
+                foreach (var item in result)
+                {
+                    System.Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("No result");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Serviices/DrugWebAPI.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DrugsService.Serviices
+{
+    public class DrugWebAPI
+    {
+        #region with tunnel
+        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
+        private static string serverIP = "israeldrugs.health.gov.il";
+        private HttpClient client;
+        private string baseUrl;
+        public static string BaseAddress = "https://israeldrugs.health.gov.il";
+        #endregion
+
+        public DrugWebAPI() 
+        {
+            //Set client handler to support cookies!!
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.CookieContainer = new System.Net.CookieContainer();
+            this.client = new HttpClient(handler);
+            this.baseUrl = BaseAddress;
+        }
+        
+        public async Task<ExpandoObject> SearchByName(ExpandoObject input)
+        {
+            //Set URI to the specific function API
+            string url = $"{this.baseUrl}/GovServiceList/IDRServer/SearchByName";
+            try
+            {
+                //Call the server API
+                string json = JsonSerializer.Serialize(input);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                //Check status
+                if (response.IsSuccessStatusCode)
+                {
+                    //Extract the content as string
+                    string resContent = await response.Content.ReadAsStringAsync();
+                    //Desrialize result
+                    JsonSerializerOptions options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                    ExpandoObject? result = JsonSerializer.Deserialize<ExpandoObject>(resContent, options);
+                    return result;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+    }
+}
